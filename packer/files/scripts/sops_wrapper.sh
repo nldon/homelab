945b200e898c6ab89e477f9e345ffe96cf56e82e
nld91 <nathanleedonovan@gmail.com>
2025-01-27 23:15:22 +0000

    Changed wrapper script to use fixed path in repo, added encrypted ansible credentials

--- packer/files/scripts/sops_wrapper.sh
+++ packer/files/scripts/sops_wrapper.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-SOPS_FILE="~/.config/credentials/ansible.yaml"
+SCRIPT_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sops decrypt "$SOPS_FILE" | ansible-playbook --vault-password-file=/bin/cat "$@"
+ANSIBLE_CREDENTIALS="$SCRIPT_DIR/../../../ansible/credentials/credentials.yml"
+
+sops decrypt --extract '["vault_password"]' "$ANSIBLE_CREDENTIALS" | ansible-playbook --vault-password-file=/bin/cat "$@"
